4f07400eab1a5001d240841306cf7d563d6a144f
SanthanaKumar <santhanam@softsolutions4u.info>
2015-08-21 13:18:44 +0530

    Added filter to get tile text.

--- js/filters.js
+++ js/filters.js
@@ -3,4 +3,36 @@ angular.module("starter.filters", [])
             return function (text) {
                 return $sce.trustAsHtml(text);
             };
+        }])
+        .filter("getTileText", [function () {
+            return function (text, letterCount) {
+                if (text === '') {
+                    return ' ';
+                }
+                var words    = text.split(' ');
+                var tileText = '';
+                for (i=0;i<letterCount;i++) {
+                    if (words[i] !== undefined) {
+                        tileText += words[i].charAt(0);
+                    } else {
+                        break;
+                    }
+                }
+                return tileText;
+            };
+        }])
+        .filter("getRandomTileStyle", [function () {
+            return function (type) {
+                var Styles = new Array();
+                Styles['square']   = [
+                    {'background-color':'blue'},
+                    {'background-color':'red'},
+                    {'background-color':'pink'}
+                ];
+                
+                var styleByType = Styles[type];
+                var style = styleByType[Math.floor(Math.random() * styleByType.length)];
+                
+                return style;
+            };
         }]);
